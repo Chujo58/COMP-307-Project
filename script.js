3cdc531bce6c9df768bc14d01fa8b66e5bcb8bff
Chujo58 <92330130+Chujo58@users.noreply.github.com>
2024-11-26 14:59:50 -0500

    navbar fix

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function toggleActive(id, ids){
     var elemToActivate = document.getElementById(id).classList;    
-    if (elemToActivate.contains("active")){
-        elemToActivate.remove("active");
-    }
-    else {
+    if (!elemToActivate.contains("active")) { //If the active class tag isn't present, add it. We only remove
         elemToActivate.add("active");
     }
+
     document.getElementById(id).classList = elemToActivate;
 
     ids.forEach(element => {

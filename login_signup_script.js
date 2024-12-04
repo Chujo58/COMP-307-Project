c18382ff58d0d4f6f8a510da79313345a27e7598
Chujo58 <92330130+Chujo58@users.noreply.github.com>
2024-12-03 22:47:04 -0500

    going to start adding some encryption if possible tonight or tomorrow

--- login_signup_script.js
+++ login_signup_script.js
@@ -1,63 +1,116 @@
 function getValue(id){
-    return document.getElementById(id).value ?? '';
+	return document.getElementById(id).value ?? '';
 }
 
 function addClass(id, text, class_text){
-    var elem = document.getElementById(id);
-    elem.classList.add(class_text);
-    if (id.includes("_div")){
-        elem.innerHTML = class_text.substring(0,1).toUpperCase() + class_text.substring(1,class_text.length) + " " + text;
-    }
+	var elem = document.getElementById(id);
+	elem.classList.add(class_text);
+	if (id.includes("_div")){
+		elem.innerHTML = class_text.substring(0,1).toUpperCase() + class_text.substring(1,class_text.length) + " " + text;
+	}
 }
 
 function removeClass(id, class_text){
-    var elem = document.getElementById(id);
-    if (elem.classList.contains(class_text)) elem.classList.remove(class_text);
-    if (id.includes("_div")){
-        elem.innerHTML = "";
-    }
+	var elem = document.getElementById(id);
+	if (elem.classList.contains(class_text)) elem.classList.remove(class_text);
+	if (id.includes("_div")){
+		elem.innerHTML = "";
+	}
 }
 
 function onLoginKeyPress(){
-    var user = getValue('username');
-    var pass = getValue('password');
+	var user = getValue('username');
+	var pass = getValue('password');
 
-    if (user.length == 0){
-        addClass('username','username','missing');
-        addClass('username_div','username','missing');
-    }
-    if (pass.length == 0){
-        addClass('password','password','missing');
-        addClass('password_div','password','missing');
-    }
-    if (user.length != 0) {
-        removeClass('username', 'missing');
-        removeClass('username_div','missing');
-    }
-    if (pass.length != 0) {
-        removeClass('password', 'missing');
-        removeClass('password_div','missing');
-    }
-    return [user, pass];
+	if (user.length == 0){
+		addClass('username','username','missing');
+		addClass('username_div','username','missing');
+	}
+	if (pass.length == 0){
+		addClass('password','password','missing');
+		addClass('password_div','password','missing');
+	}
+
+	if (user.length != 0) {
+		removeClass('username', 'missing');
+		removeClass('username_div','missing');
+	}
+	if (pass.length != 0) {
+		removeClass('password', 'missing');
+		removeClass('password_div','missing');
+	}
+	return [user, pass];
 }
 
+function onSignUpKeyPress(){
+	var user = getValue('username');
+	var pass = getValue('password');
+	var c_pass = getValue('confirm_password');
 
+	if (user.length == 0){
+		addClass('username','username','missing');
+		addClass('username_div','username','missing');
+	}
+	if (pass.length == 0){
+		addClass('password','password','missing');
+		addClass('password_div','password','missing');
+	}
+	if (c_pass.length == 0){
+		addClass('confirm_password','password','missing');
+		addClass('confirm_password_div','password','missing');
+	}
+
+	if (user.length != 0) {
+		removeClass('username', 'missing');
+		removeClass('username_div','missing');
+	}
+	if (pass.length != 0) {
+		removeClass('password', 'missing');
+		removeClass('password_div','missing');
+	}
+	if (c_pass.length != 0){
+		removeClass('confirm_password', 'missing');
+		removeClass('confirm_password_div','missing');
+	}
+	return [user, pass, c_pass];
+}
 
 function onLoginClick(){
-    var user = "";
-    var pass = "";
-    data = onLoginKeyPress();
-    user = data[0];
-    pass = data[1];
+	var user = "";
+	var pass = "";
+	data = onLoginKeyPress();
+	user = data[0];
+	pass = data[1];
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if (this.readyState == 4 && this.status == 200){
-            document.getElementById('test').innerHTML = this.responseText;
-        }
-    }
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200){
+			document.getElementById('test').innerHTML = this.responseText;
+		}
+	}
 
-    xhttp.open("POST", "php/login.php", "true");
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send(`username=${user}&password=${pass}`);
+	xhttp.open("POST", "php/login.php", "true");
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send(`username=${user}&password=${pass}`);
+}
+
+function onSignUpClick(){
+	var user = "";
+	var pass = "";
+	var c_pass = "";
+	data = onSignUpKeyPress();
+	user = data[0];
+	pass = data[1];
+	c_pass = data[2];
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200){
+			document.getElementById('test').innerHTML = this.responseText;
+		}
+	}
+
+	xhttp.open("POST","php/signup.php","true");
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send(`username=${user}&password=${pass}&confirm_password=${c_pass}`);
 }
---
+++ on_page_load.js
@@ -0,0 +1,17 @@
+const home = `<a class="site-navbar nav-item active" href="#/!" id="home" onclick="navbarClick('home');">Home</a>`
+const signup = `<a class="utility-navbar nav-item"  id="signup" onclick="navbarClick('signup');">Sign Up</a>`
+const login = `<a class="utility-navbar nav-item"  id="login" onclick="navbarClick('login');">Login</a>`
+
+
+
+function loadNavbar(){
+    let navbar = document.getElementById("navbar");
+    let site_navbar = document.getElementById("site-navbar");
+    let utility_navbar = document.getElementById("utility-navbar");
+
+    site_navbar.innerHTML = home;
+    utility_navbar.innerHTML = signup + login;
+}
+
+
+window.addEventListener("load",loadNavbar);

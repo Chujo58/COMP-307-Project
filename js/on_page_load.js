32edffcf74dbb0d0264875d6cc085a6a20f55f06
Chujo58 <92330130+Chujo58@users.noreply.github.com>
2024-12-18 23:17:48 -0500

    staff and nav fix

--- js/on_page_load.js
+++ js/on_page_load.js
@@ -1,7 +1,7 @@
 const menu = `<a class="site-navbar" id="navbar-menu"><img src="./icons/icons8-menu-win.svg"></a>`
 const logo = `<a class="site-navbar" href="./index.php?Page=Home" id="logo" onclick="navbarClick('home');"><img src="./icons/307logo_fixed.svg"></a>`
 const home = `<a class="site-navbar nav-item" href="./index.php?Page=Home" id="home" onclick="navbarClick('home');">Home</a>`
-const calendar = `<a class="site-navbar nav-item" href="./index.php?Page=Calendar" id="calendar" onclick="navbarClick('calendar');">Calendar</a>`
+const calendar = `<a class="site-navbar nav-item" href="./index.php?Page=Calendar&session=true" id="calendar" onclick="navbarClick('calendar');">Calendar</a>`
 const dashboard = `<a class="site-navbar nav-item" href="./index.php?Page=Dashboard" id="dashboard" onclick="navbarClick('dashboard');">Dashboard</a>`
 const signup = `<a class="utility-navbar nav-item"  id="signup" onclick="navbarClick('signup');">Sign Up</a>`
 const login = `<a class="utility-navbar nav-item"  id="login" onclick="navbarClick('login');">Login</a>`
